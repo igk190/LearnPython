ef92f40fb501cd1a660c8ea40a56a560e53886a5
gracia <graciakleijnen@hotmail.com>
2016-09-11 10:50:27 +0200

    Added ex32 test file.

---
+++ ex32_test.py
@@ -0,0 +1,11 @@
+for i in range(0, 10, 1):
+	if i == 1:
+		print "Today I drank one beer."
+	elif i == 2:
+		print "Today I drank two beers."
+	else:
+		print "Today I drank all the beers! {0} and counting".format(i)
+
+
+for i in range(0, 10, 1):
+	print "Ik ben een test met nummer {0}".format(i)
